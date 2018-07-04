5fcca83cfe82cd1d7f84d47f6182f69c8a045f6d
Chris Chuang <chrischuang0421@gmail.com>
2018-07-04 22:47:29 +0800

    Home page complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import 'styles/Home.scss';
 
 import COVER_IMG from 'images/comic_cover.png';
@@ -32,16 +33,30 @@ const Home = () => {
               <h2>Summary</h2>
               <p>
                 If your banker breaks, you snap; if your apothecary by mistake
-                sends you poison in your pills, you die. 
+                sends you poison in your pills, you die.
               </p>
               <p>
-              Therefore, I say, I
-              saw that this situation of mine was the precise situation of
-              every mortal that has this Siamese connexion with a plurality of other mortals.
+                Therefore, I say, I saw that this situation of mine was the
+                precise situation of every mortal that has this Siamese
+                connexion with a plurality of other mortals.
               </p>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="chapter-area">
+        <div>
+          <h2>All Chapters</h2>
+        </div>
+        <ul className="chapter-list">
+          <li>
+            <NavLink to="/my_hexschool/ch1/1">Chapter 1: The F2E Challenge Start!</NavLink>
+          </li>
+          <li>
+          <NavLink to="/my_hexschool/ch2/1" className="new">Chapter 2: Todo List is Going Crazy!</NavLink>
+          </li>
+        </ul>
       </div>
     </div>;
 }
